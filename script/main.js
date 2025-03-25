74e68dba4c728532d05149e7dfde954702551547
doverglide <netesov2333@mail.ru>
2025-03-26 01:04:46 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -2,148 +2,126 @@ let toDoForm = document.querySelector('#form');
 let toDoInput = document.querySelector('#input');
 let toDoBtn = document.querySelector('#btn');
 let toDoList = document.querySelector('#list');
-let completeList = document.querySelector('.complete__list');
+let completeList = document.querySelector('.container__list');
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+};
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (tasks.length > 0) {
+        tasks.forEach(task => {
+            cssClass = task.done ? 'task__value task__value-task--done' : 'task__value'; 
+            
+            const HTML = `<div class="task" id="${task.id}">
+            <div class="task__content">
+            ${task.done ? '' : '<input type="checkbox">'}
+            <li class="${cssClass}">${task.text}</li>
+            </div>
+            <button type="button" class="btn btn--remove">Remove</button>
+            </div>`
+            
+            toDoList.insertAdjacentHTML("beforeend", HTML); 
+        });
+    }
+
+    removeTask();
+    doneTask();
+});
 
 toDoForm.addEventListener('submit', function(event) {
     event.preventDefault();
-
-    let taskText = toDoInput.value;
-    let newTask = createTask(taskText);
-
-    toDoList.appendChild(newTask);
     
-    toDoInput.value = '';
-    toDoInput.focus();
-
-    return taskText;
+    taskText = toDoInput.value; 
+    
+    createTask(taskText);
 });
 
 function createTask(taskText) {
-    let taskWrap = document.createElement('div');
-    taskWrap.classList.add('li-wrapper');
+    
+    const newTaskObject = {
+        id: Date.now(),
+        text: taskText,
+        done: false
+    };
 
-    let taskContent = document.createElement('div');
-    taskContent.classList.add('li-wrapper__content');
+    createHTML(taskText, newTaskObject);
+    removeTask();
+    doneTask();
 
-    let taskControls = document.createElement('div');
-    taskControls.classList.add('li-wrapper__btns');
+    toDoInput.value = '';
 
-    let newTask = document.createElement('li');
-    newTask.textContent = taskText;
-
-    let checkBox = handleCompletion(taskWrap, newTask);
-    let removeBtn = removeTask(taskWrap);
-    let editBtn = editTask(taskContent, taskControls, newTask, removeBtn, checkBox);
-
-    taskContent.appendChild(checkBox);
-    taskContent.appendChild(newTask);
-
-    taskControls.appendChild(editBtn);
-    taskControls.appendChild(removeBtn);
-
-    taskWrap.appendChild(taskContent);
-    taskWrap.appendChild(taskControls);
-
-    taskWrap.style.opacity = '0';
-
-    requestAnimationFrame(() => {
-        taskWrap.style.opacity = '1';
-    });
-
-
-    return taskWrap;
+    tasks.push(newTaskObject);
+    
+    saveToLocalStorage();
 };
 
-function removeTask(taskWrap) {
-    let removeBtn = document.createElement('button');
-    removeBtn.setAttribute('type', 'button');
-    removeBtn.classList.add('btn', 'btn--remove');
-    removeBtn.textContent = 'Remove';
+function createHTML(taskText, newTaskObject) {
 
-    removeBtn.addEventListener('click', function() {
-        taskWrap.style.opacity = '0';
-    
-        setTimeout(() => {
-            taskWrap.remove();
-        }, 250);
-    });
-    
-    return removeBtn;
+    cssClass = newTaskObject.done ? 'task__value task__value-task--done' : 'task__value';
+
+    const taskHTML = `<div class="task" id="${newTaskObject.id}">
+            <div class="task__content">
+            <input type="checkbox">
+            <li class="${cssClass}">${taskText}</li>
+            </div>
+            <button type="button" class="btn btn--remove">Remove</button>
+            </div>`
+
+        toDoList.insertAdjacentHTML('beforeend', taskHTML);
 };
 
-function handleCompletion(taskWrap, newTask) {
-    let checkBox = document.createElement('input');
-    checkBox.setAttribute('type', 'checkbox');
+function removeTask() {
+    let removeBtns = document.querySelectorAll('.btn--remove');
 
-    checkBox.addEventListener('change', function() {
-        if (checkBox.checked) {
-            newTask.style.color = 'green';
-            newTask.style.textDecoration = 'line-through';
-    
-            let completeTextTask = newTask;
-            let completeTask = document.createElement('li');
-    
-            setTimeout(() => {
-                taskWrap.style.opacity = '0';
-    
-                setTimeout(() => {
-                    taskWrap.remove();
-    
-                    completeTask.appendChild(completeTextTask);
-                    completeList.appendChild(completeTask);
-    
-                    completeTask.style.opacity = '1';
-                }, 600);
-            }, 400);
-    
-        } else {
-            newTask.style.color = 'black';
-            newTask.style.textDecoration = 'none';
+    removeBtns.forEach(btn => {
+        btn.onclick = function() {
+            let taskElement = this.closest('.task');
+            
+            if (taskElement) {
+                taskElement.remove();
+
+                const taskId = Number(taskElement.getAttribute('id'));
+
+                tasks = tasks.filter(task => task.id !== taskId);
+
+                saveToLocalStorage();
+            }
         }
     });
-
-    return checkBox;
 };
 
-function editTask(taskContent,  taskControls, newTask, removeBtn, checkBox) {
-    let editBtn = document.createElement('button');
-    editBtn.setAttribute('type', 'button');
-    editBtn.classList.add('btn', 'btn--edit');
-    editBtn.textContent = 'Edit';
+function doneTask () {
+    let doneBtns = document.querySelectorAll('input[type="checkbox"]');
+    
+    doneBtns.forEach(btn => {
+        btn.addEventListener('change', function() {
+            let taskTextElement = this.closest('.task').querySelector('.task__value');
 
-    editBtn.addEventListener('click', function() {
-        checkBox.remove();
-        removeBtn.remove();
-    
-        let saveBtn = document.createElement('button');
-        saveBtn.setAttribute('type', 'button');
-        saveBtn.classList.add('btn', 'btn--save');
-        saveBtn.innerHTML = 'Save';
-    
-        let inputEdit = document.createElement('input');
-        inputEdit.setAttribute('type', 'text');
-        inputEdit.classList.add('input--edit');
-        inputEdit.value = newTask.textContent;
-    
-        taskContent.style.width = '85%';
-    
-        taskContent.replaceChild(inputEdit, newTask);
-        taskControls.replaceChild(saveBtn, editBtn);
-    
-        saveBtn.addEventListener('click', function() {
-            saveBtn.remove();
-    
-            newTask.textContent = inputEdit.value;
-    
-            taskContent.replaceChild(newTask, inputEdit);
-    
-            taskContent.appendChild(checkBox);
-            taskControls.appendChild(editBtn);
-            taskControls.appendChild(removeBtn);
-    
-            taskContent.replaceChildren(checkBox, newTask);
+            let taskElement = this.closest('.task').getAttribute('id');
+
+            if (btn.checked) {
+                taskTextElement.classList.add('task__value-task--done');
+
+                btn.remove();
+
+                const taskId = Number(taskElement);
+
+                const task = tasks.find(task => task.id === taskId);
+
+                if (task) {
+                    task.done = true;
+                }
+
+                saveToLocalStorage();
+            } 
         });
     });
-
-    return editBtn;
 };
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
